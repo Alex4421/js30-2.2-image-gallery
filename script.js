17534b4a1b84d61bf7ddbf28ad45bbe0de407fe1
Alex4421 <cherevko.alex96@gmail.com>
2024-09-24 10:32:16 +0300

    feat: add footer

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let pictures = Array.from(document.getElementsByClassName('picture'));
 const search = document.getElementById('search-input');
 const apiKey = 'SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
 
-async function getData() {
+async function getData(query = '') {
   try {
-    let url = `https://api.unsplash.com/search/photos?query=${search.value}&client_id=${apiKey}&per_page=12`;
+    let url = `https://api.unsplash.com/search/photos?query=${query}&client_id=${apiKey}&per_page=12`;
     const res = await fetch(url);
     const data = await res.json();
     console.log(data);
@@ -21,22 +21,28 @@ async function getData() {
     console.error('Ошибка при получении данных:', error);
   }
 }
-const button = document.getElementById('icon-search')
 
-// навешиваем обработчик на событие клик
+const button = document.getElementById('icon-search');
 button.addEventListener('click', function () {
-    getData()
+    getData(search.value)
 });
 
 document.addEventListener("DOMContentLoaded", function() {
+    getData('зима');
   const searchInput = document.getElementById("search-input");
   searchInput.autocomplete = "off";
   searchInput.focus();
+  searchInput.value = '';
 
   searchInput.addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
-      getData();
+      getData(search.value);
     }
   });
 });
-// при онклоике брать значение value импуте вызывать getData
+
+const clearButton = document.querySelector('.clear-button');
+clearButton.addEventListener('click', () => {
+    const searchInput = document.getElementById("search-input");
+  searchInput.value = '';
+});
